2fb1c0e79544d18a34f2ed6ebe39cd38c4894613
googleyufei <googleyufei@qq.com>
2015-09-28 16:36:38 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# qycloud-postpackager-seajs
+
+a postpackager plugin for fis to create sea-config.js.
+
+## usage
+    $ npm isstall -g qycloud
+    $ vi path/to/project/fis-conf.js
+
+```javascript
+//file : path/to/project/fis-conf.js
+fis.config.set('modules.postpackager', 'seajs');
+```
---
+++ index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = function(ret, settings, conf, opt) { //打包后处理
+    var fis_sea_conf = fis.config.get('seajs', {});
+    fis_sea_conf.alias = fis_sea_conf.alias || {};
+    fis.util.map(ret.map.res, function(id, res) {
+        fis_sea_conf.alias[id] = res.uri;
+    });
+    var content = 'seajs.config(' + JSON.stringify(fis_sea_conf, null, opt.optimize ? null : 4) + ');';
+    var seajs_config = fis.file(fis.project.getProjectPath(), 'sea-config.js');
+    seajs_config.setContent(content);
+    ret.pkg[seajs_config.subpath] = seajs_config;
+
+    var script = '<script src="' + seajs_config.getUrl(opt.hash, opt.domain) + '"></script>';
+    fis.util.map(ret.src, function(subpath, file) {
+        if (file.isHtmlLike) { //类html文件
+            var content = file.getContent();
+            if (/\bseajs\.use\s*\(/.test(content)) { //如果有sea.use(，才会插入
+                //插入到页面</head>标签结束之前
+                content = content.replace(/<\/head>/, script + '\n$&');
+                file.setContent(content);
+            }
+        }
+    });
+};
